9119e1d5cc7ec143967d0e1e22bfa2acd7dbb536
ShawnLSmith <shawn@rsatechnologyservices.com>
2022-09-09 17:04:24 -0500

    Finished the Hundo Project
    Following the Mini-Site Template parts 1-4 & Building the hundo videos. We built the design of the website, used CSS /Bootstrap to modify the look of it, and typed in the JavaScript for the app itself. We then added the code text from the site.js to the code page via prism.js so that the user could look over the essential code of the Hundo Application. Then I commented out the social media platforms I currently don't use, and plan on uploading a new logo and my current social media platforms later.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,67 @@
-//Call HelloWorld
-function HelloWorld(){
-    alert("Hello World");
+//Get the Values from the interface
+//Starts our controller function
+function getValues() {
+
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    let numbers = [];
+
+    //we need to validate our input
+    //Parse into Integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //we call generateNumbers
+        numbers = generateNumbers(startValue, endValue);
+        //we call displayNumbers
+        displayNumbers(numbers);
+    }
+    else {
+        alert("You must enter integers");
+    }    
+
+}
+
+//Generate numbers fro the startValue to the endValue
+//Logic function(s)
+function generateNumbers(sValue, eValue){    
+        
+    let numbers = [];
+    
+    //we want to get all numbers from start to end.
+    //index = index + 1; >>>>> same as index++
+    for(let index = sValue; index <= eValue; index++){
+        
+        //This will execute in a loop until index = endValue.
+        numbers.push(index);
+    }        
+    
+    return numbers;    
+}
+
+//Display the numbers and mark even numbers bold
+//Display or view function(s)
+function displayNumbers(numbers){
+    
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+
+        let className ="even";
+
+        let number = numbers[index];
+
+        if(number % 2 == 0){
+            className = "even";
+        }
+        else{
+            className = "odd";
+        }
+        templateRows += `<tr><td class="${className}">${number}</tr></td>`;
+
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
 }
